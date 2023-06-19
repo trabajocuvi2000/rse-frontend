2595bf12ae12d4d8deffb8899d6fc0cc9bfc3bdd
trabajocuvi2000 <trabajocuvi@gmail.com>
2023-06-19 15:52:13 -0500

    rsef

--- js/urls.js
+++ js/urls.js
@@ -1,5 +1,5 @@
 // var url_global_pagina = 'https://r-s-e-evaluacion-ucuenca.herokuapp.com/html/'; // esto estaba antes
-var url_global_pagina = 'http://127.0.0.1:5501/html/';
+var url_global_pagina = 'https://sparkly-gecko-d2c6b6.netlify.app/html/';
 
 // var link_service = `https://heruko-postgress.herokuapp.com/ejemplo01/`
 // var link_service = `https://back-end-postgres-rse.herokuapp.com/` // this was before 
