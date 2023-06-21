c07dda93d451fe69fdc9a9a0099a48a1230441cd
trabajocuvi2000 <trabajocuvi@gmail.com>
2023-06-21 14:37:23 -0500

    rsef

--- js/urls.js
+++ js/urls.js
@@ -1,5 +1,5 @@
-// var url_global_pagina = 'https://rse-evaluacion.netlify.app/html/'; // nube 
-var url_global_pagina = 'http://127.0.0.1:5501/html/'; // local
+var url_global_pagina = 'https://rse-evaluacion.netlify.app/html/'; // nube 
+// var url_global_pagina = 'http://127.0.0.1:5501/html/'; // local
 
 // var link_service = `http://localhost:8080/` // this was before 
 var link_service = `http://rse-server-env.eba-tf5vfmpd.us-east-2.elasticbeanstalk.com/` // this was before 
