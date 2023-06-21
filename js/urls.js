ea9fcf8617167c9fb806d0741fe45ca174f0ea56
trabajocuvi2000 <trabajocuvi@gmail.com>
2023-06-21 14:36:37 -0500

    rsef

--- js/urls.js
+++ js/urls.js
@@ -1,7 +1,6 @@
-// var url_global_pagina = 'https://r-s-e-evaluacion-ucuenca.herokuapp.com/html/'; // esto estaba antes
-var url_global_pagina = 'https://rse-evaluacion.netlify.app/html/';
+// var url_global_pagina = 'https://rse-evaluacion.netlify.app/html/'; // nube 
+var url_global_pagina = 'http://127.0.0.1:5501/html/'; // local
 
-// var link_service = `https://heruko-postgress.herokuapp.com/ejemplo01/`
-// var link_service = `https://back-end-postgres-rse.herokuapp.com/` // this was before 
-var link_service = `http://localhost:8080/` // this was before 
+// var link_service = `http://localhost:8080/` // this was before 
+var link_service = `http://rse-server-env.eba-tf5vfmpd.us-east-2.elasticbeanstalk.com/` // this was before 
 var extencion = `.html`
